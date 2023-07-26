48e4fc8467b1238b715802c9bebee215bdc276e2
Vikash Pirthiani <vikash.pirthiani@gmail.com>
2023-07-26 23:49:10 +0200

    test

--- app/router/router_tasks.py
+++ app/router/router_tasks.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 
 @router.post("/tasks/")
-def create_task(title: str,status: str, description: str = None):
+def create_task(title: str, status: str, description: str = None):
     db = SessionLocal()
     task = Task(title=title, description=description, status=status)
     db.add(task)
